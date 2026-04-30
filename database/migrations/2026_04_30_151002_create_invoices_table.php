cae7a4a2471a43a53ea370387a64dc103fa722fc
Zedek <listermatrix360@gmail.com>
2026-04-30 15:15:44 +0000

    initiated repo , added basic setup(user,invoice,customer,sanctum,product)

---
+++ database/migrations/2026_04_30_151002_create_invoices_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('invoices', static function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('customer_id')->constrained()->cascadeOnDelete();
+            $table->string('invoice_number')->unique();
+            $table->date('issue_date');
+            $table->date('due_date');
+            $table->enum('status', ['draft', 'sent', 'paid', 'overdue', 'cancelled'])->default('draft');
+            $table->text('notes')->nullable();
+            $table->decimal('total_amount', 12, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
